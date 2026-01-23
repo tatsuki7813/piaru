69c07b57819f4c4b200d0691d08e6d6b854c3957
kobayashi tatsuki <kobattk782@gmail.com>
2026-01-23 22:50:17 +0900

    add logout module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :logged_in?
+
+  private
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
