88eda1306a60a399794bea3258f6ac8d51f3dbdb
kobayashi tatsuki <kobattk782@gmail.com>
2026-01-23 23:47:13 +0900

    add authenticate before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,23 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
+
+  before_action :authenticate
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
