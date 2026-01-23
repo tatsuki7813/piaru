a93aa9cddc97ff4ac6d0782a646a2f0366cf87a1
kobayashi tatsuki <kobattk782@gmail.com>
2026-01-23 22:40:22 +0900

    apply bootstrap and fix notice text

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_path, notice: "Successfully logged in."
+    redirect_to root_path, notice: "ログインしました"
   end
 end
