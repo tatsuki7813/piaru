034e672baccc1b8689f8888dbe32d37ca21f0700
kobayashi tatsuki <kobattk782@gmail.com>
2026-01-28 22:53:39 +0900

    implement user event creation functionality by adding controller actions and a user-event association.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :place, :content, :start_at, :end_at)
+  end
 end
